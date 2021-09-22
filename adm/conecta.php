7ad6a543d5c3721c42b92bc4bbe934274e5f16f9
Thiago <contato@novaeraweb.com.br>
2021-09-22 10:14:01 -0300

    Test

--- adm/conecta.php
+++ adm/conecta.php
@@ -3,8 +3,11 @@
 if ($URL_ATUAL=='www.lelopagani.com.br') {
 	$lelo = mysqli_connect('mysql745.umbler.com', 'lelopagani_user', 'lp*010203', 'lelopagani');
 } else {
-	$lelo = mysqli_connect('localhost', 'root', 'root', 'lelopagani'); 
+	$lelo = mysqli_connect('localhost', 'root', 'root', 'lelopagani');
+
 }
+echo $lelo;
+exit;
 mysqli_query($lelo, "SET NAMES 'utf8'");
 mysqli_query($lelo,'SET character_set_connection=utf8');
 mysqli_query($lelo,'SET character_set_client=utf8');
